e958ba9300106f06c162dd85202058d743dd63d6
Jiapapneja <jiaapapneja22@gmail.com>
2025-02-05 15:31:36 +0530

    Solution to Check Even or Odd

--- Check Even or Odd@@6788dd63490637cfc44e1b8d/code.c
+++ Check Even or Odd@@6788dd63490637cfc44e1b8d/code.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(){
-    int c;
-    scanf("%d",&c);
-    if(c%5==0 && c%11==0){
-        printf("Divisible");
+    int a;
+    scanf("%d",&a);
+    if(a%2==0){
+        printf("Even");       
     }
     else{
-        printf("Not Divisible");
+        printf("Odd");
     }
     return 0;
 }
